5d4626d2d38633b77207358653df4cbafb4b09f9
webmalc <m@webmalc.pw>
2020-02-29 17:25:33 +0300

    Add the ability to load configuration files based on the environment variable

--- config/config.go
+++ config/config.go
@@ -1,16 +1,31 @@
 package config
 
 import (
+	"os"
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
+const prefix = "its"
+
+// getFilename returns filename based on the environment variable
+func getFilename() string {
+	fileName := "config"
+	env := os.Getenv(strings.ToUpper(prefix + "_env"))
+	if env != "" {
+		fileName += "." + env
+	}
+	return fileName
+}
+
 // Init initializes the main configuration.
 func Setup() {
-	viper.SetConfigName("config")
+	viper.SetConfigName(getFilename())
 	viper.AddConfigPath("./config")
 	viper.AddConfigPath("/etc/itstats")
 	viper.AddConfigPath("$HOME/.itstats")
-	viper.SetEnvPrefix("its")
+	viper.SetEnvPrefix(prefix)
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
--- main_test.go
+++ main_test.go
@@ -3,10 +3,18 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/webmalc/it-stats-backend/config"
 )
 
+// Setups the tests
+func setUp() {
+	os.Setenv("ITS_ENV", "test")
+	config.Setup()
+}
+
 func TestMain(m *testing.M) {
-	// setup()
+	setUp()
 	code := m.Run()
 	// shutdown()
 	os.Exit(code)
